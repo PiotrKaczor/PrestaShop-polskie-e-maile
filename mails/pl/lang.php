65eb994b58920fdb112e7d082ab531a29ec7cd92
Piotr Kaczor <piokaczor@gmail.com>
2013-06-18 10:04:59 +0200

    Poprawia braki w tłumaczeniach Tematów e-maili
    close #2 close #12 close #13 close #29

---
+++ mails/pl/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+global $_LANGMAIL;
+$_LANGMAIL = array();
+
+$_LANGMAIL['Message from contact form'] = 'Wiadomość z formularza kontaktowego';
+$_LANGMAIL['Your message has been correctly sent'] = 'Przesłano wiadomość';
+$_LANGMAIL['Your message has been correctly sent #ct%1$s #tc%2$s'] = 'Przesłano wiadomość #ct%1$s #tc%2$s';
+$_LANGMAIL['Order confirmation'] = 'Podsumowanie zamówienia';
+$_LANGMAIL['Referral Program'] = 'Dołącz do nas';
+$_LANGMAIL['New order - #%06d'] = 'Masz nowe zamówienie - #%06d';
+$_LANGMAIL['Product out of stock'] = 'Produkt na wyczerpaniu';
