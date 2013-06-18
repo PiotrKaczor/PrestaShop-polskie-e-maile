6e79cfb9f5bef08b7f552c333d63d1b1f3ca0ab2
Piotr Kaczor <piokaczor@gmail.com>
2013-06-19 01:09:58 +0200

    Struktura tłumaczenia odpowiadająca plikowi eng
    Łatwiej można porównać pliki PL vs EN

--- mails/pl/lang.php
+++ mails/pl/lang.php
@@ -1,43 +1,75 @@
 <?php
+/*
+* 2007-2013 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Open Software License (OSL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/osl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author PrestaShop SA <contact@prestashop.com>
+*  @copyright  2007-2013 PrestaShop SA
+*  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
 
 global $_LANGMAIL;
 $_LANGMAIL = array();
-
-$_LANGMAIL['Newsletter confirmation'] = 'Potwierdzenie newsletter';
-$_LANGMAIL['Email verification'] = 'E-mail weryfikacyjny';
-$_LANGMAIL['Newsletter voucher'] = 'Voucher za newsletter';
+$_LANGMAIL['Welcome!'] = 'Witaj!';
+$_LANGMAIL['Message from contact form'] = 'Wiadomość z formularza kontaktowego';
+$_LANGMAIL['Your message has been correctly sent'] = 'Przesłano wiadomość';
+$_LANGMAIL['New credit slip regarding your order'] = 'Nowy \'kredyt\' dotyczący zamówienia';
+$_LANGMAIL['Virtual product to download'] = 'Wirtualny produkt do pobrania';
+$_LANGMAIL['Fwd: Customer message'] = 'TR: Wiadomość od klienta';
+$_LANGMAIL['Your guest account has been transformed to customer account'] = 'Twoje konto \'Gość\' zostało przekształcone w konto \'Klient\'';
+$_LANGMAIL['Package in transit'] = 'Zamówienie wysłane';
+// $_LANGMAIL['[Log'] = '[Log';
+$_LANGMAIL['Order confirmation'] = 'Podsumowanie zamówienia';
+$_LANGMAIL['Message from a customer'] = 'Wiadomość od klienta';
+$_LANGMAIL['New message regarding your order'] = 'Nowa wiadomość dotycząca zamówienia';
+$_LANGMAIL['Your order return state has changed'] = 'Nowy status zamówienia';
+// $_LANGMAIL['Your password'] = 'Your password has been reset';
+$_LANGMAIL['Password query confirmation'] = 'Potwierdzenie prośby o hasło';
+// $_LANGMAIL['An answer to your message is available'] = 'New response to your inquiry';
+$_LANGMAIL['New voucher regarding your order %s'] = 'Nowy kod rabatowy dotyczący zamówienia %s';
+// $_LANGMAIL['Happy birthday!'] = 'Happy birthday!';
+$_LANGMAIL['Newsletter confirmation'] = 'Potwierdzenie Newslettera';
+$_LANGMAIL['Newsletter voucher'] = 'Kod rabatowy za Newsletter';
+$_LANGMAIL['Your wishlist\\\'s link'] = 'Link do Twojej listy życzeń';
+// $_LANGMAIL['Message from '] = 'Message from ';
+// $_LANGMAIL['$subject'] = '';
 $_LANGMAIL['Your cart and your discount'] = 'Twój koszyk i Twój kupon rabatowy';
 $_LANGMAIL['Thanks for your order'] = 'Dziękujemy za dokonanie zamówienia';
 $_LANGMAIL['You are one of our best customers'] = 'Jesteś jednym z naszych najlepszych klientów';
 $_LANGMAIL['We miss you'] = 'Tęsknimy za Tobą';
 $_LANGMAIL['Product available'] = 'Dostępny produkt';
-$_LANGMAIL['Stock coverage'] = 'Aktualne pokrycie stanu';
 $_LANGMAIL['Product out of stock'] = 'Wyczerpanie zapasów';
+// $_LANGMAIL['Error reporting from your PayPal module'] = 'Error reporting from your PayPal module';
 $_LANGMAIL['Congratulations!'] = 'Brawo!';
-$_LANGMAIL['%1$s sent you a link to %2$s'] = '%1$s wysłał Ci link do %2$s';
-$_LANGMAIL['Welcome!'] = 'Witaj !';
+$_LANGMAIL['Referral Program'] = 'Dołącz do nas';
+// $_LANGMAIL['A friend sent you a link to'] = 'A friend sent you a link to';
+
+// Angielski plik konczy sie na lini 61
+$_LANGMAIL['Email verification'] = 'E-mail weryfikacyjny';
+$_LANGMAIL['Stock coverage'] = 'Aktualne pokrycie stanu';
+$_LANGMAIL['%1$s sent you a link to %2$s'] = '%1$s wysłanł Ci link do %2$s';
 $_LANGMAIL['Process the payment of your order'] = 'Przetwórz płatność dla Twojego zamówienia';
-$_LANGMAIL['New credit slip regarding your order'] = 'Nowy \'kredyt\' dotyczący zamówienia';
-$_LANGMAIL['Virtual product to download'] = 'Wirtualny produkt do pobrania';
-$_LANGMAIL['Your guest account has been transformed to customer account'] = 'Twoje konto \'gość\' zostało przekształcone w konto \'klient\'';
 $_LANGMAIL['Log: You have a new alert from your shop'] = 'Log: masz nowe ostrzeżenie z Twojego sklepu';
 $_LANGMAIL['Your order has been changed'] = 'Twoje zamówienie zostało zmodyfikowane';
-$_LANGMAIL['Message from a customer'] = 'Wiadomość od klienta';
-$_LANGMAIL['New message regarding your order'] = 'Nowa wiadomość dotycząca zamówienia';
-$_LANGMAIL['Your order return state has changed'] = 'Nowy status zamówienia';
 $_LANGMAIL['Your new password'] = 'Twoje nowe hasło';
-$_LANGMAIL['Password query confirmation'] = 'Potwierdzenia prośby o hasło';
 $_LANGMAIL['An answer to your message is available #ct%1$s #tc%2$s'] = 'Dostępna jest odpowiedź na Twoją wiadomość #ct%1$s #tc%2$s';
-$_LANGMAIL['New voucher regarding your order %s'] = 'Nowy kod rabatowy dotyczący zamówienia %s';
-
-$_LANGMAIL['Message from contact form'] = 'Wiadomość z formularza kontaktowego';
-$_LANGMAIL['Your message has been correctly sent'] = 'Przesłano wiadomość';
 $_LANGMAIL['Your message has been correctly sent #ct%1$s #tc%2$s'] = 'Przesłano wiadomość #ct%1$s #tc%2$s';
-$_LANGMAIL['Order confirmation'] = 'Podsumowanie zamówienia';
-$_LANGMAIL['Package in transit'] = 'Zamówienie wysłane';
-$_LANGMAIL['Referral Program'] = 'Dołącz do nas';
 $_LANGMAIL['New order - #%06d'] = 'Masz nowe zamówienie - #%06d';
 $_LANGMAIL['Product out of stock'] = 'Produkt na wyczerpaniu';
-$_LANGMAIL['Your wishlist\\\'s link'] = 'Link do Twojej listy życzeń';
 $_LANGMAIL['Message from %1$s %2$s'] = 'Wiadomość od %2$s %1$s';
-$_LANGMAIL['Fwd: Customer message'] = 'TR: Wiadomość od klienta';
