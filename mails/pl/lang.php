f355a78aa817e40d89eced7feee70d7f3e80255a
Piotr Kaczor <piokaczor@gmail.com>
2013-06-18 10:58:20 +0200

    Tytuł e-maila o wysyłce

--- mails/pl/lang.php
+++ mails/pl/lang.php
@@ -7,6 +7,7 @@ $_LANGMAIL['Message from contact form'] = 'Wiadomość z formularza kontaktowego
 $_LANGMAIL['Your message has been correctly sent'] = 'Przesłano wiadomość';
 $_LANGMAIL['Your message has been correctly sent #ct%1$s #tc%2$s'] = 'Przesłano wiadomość #ct%1$s #tc%2$s';
 $_LANGMAIL['Order confirmation'] = 'Podsumowanie zamówienia';
+$_LANGMAIL['Package in transit'] = 'Zamówienie wysłane';
 $_LANGMAIL['Referral Program'] = 'Dołącz do nas';
 $_LANGMAIL['New order - #%06d'] = 'Masz nowe zamówienie - #%06d';
 $_LANGMAIL['Product out of stock'] = 'Produkt na wyczerpaniu';
